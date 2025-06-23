19cee6e2741600d62e02697802695ce11ba4a9ba
MoYuSOwO <hhj2006@outlook.com>
2025-06-23 19:19:43 +0800

    fix: 修复碰撞箱判定

--- core/src/main/java/io/github/moyusowo/neoartisan/block/util/BoundingBoxUtil.java
+++ core/src/main/java/io/github/moyusowo/neoartisan/block/util/BoundingBoxUtil.java
@@ -2,6 +2,7 @@ package io.github.moyusowo.neoartisan.block.util;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.BoundingBox;
@@ -12,7 +13,9 @@ public class BoundingBoxUtil {
     public static boolean overlap(@NotNull final Block block) {
         final Location location = block.getLocation().add(0.5, 0.5, 0.5);
         for (Entity entity : location.getWorld().getNearbyEntities(location, 1.5, 1.5, 1.5)) {
-            if (entity.getBoundingBox().overlaps(BoundingBox.of(block))) {
+            CraftEntity craftEntity = (CraftEntity) entity;
+            net.minecraft.world.entity.Entity nmsEntity = craftEntity.getHandle();
+            if (nmsEntity.isCollidable(false) && entity.getBoundingBox().overlaps(BoundingBox.of(block))) {
                 return true;
             }
         }
