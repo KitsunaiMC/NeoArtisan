50e64c1f9e8a2868a7809e1aa1f5fef979e34a97
MoYuSOwO <hhj2006@outlook.com>
2025-07-15 12:56:07 +0800

    chore: Remove unused annotation from interface
    Deleted the @SuppressWarnings("unused") annotation from ArtisanBlockDataInternal as it was not needed.

--- core/src/main/java/io/github/moyusowo/neoartisan/block/base/internal/ArtisanBlockDataInternal.java
+++ core/src/main/java/io/github/moyusowo/neoartisan/block/base/internal/ArtisanBlockDataInternal.java
@@ -3,10 +3,8 @@ package io.github.moyusowo.neoartisan.block.base.internal;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-@SuppressWarnings("unused")
 public interface ArtisanBlockDataInternal {
     void setPersistentDataContainer(PersistentDataContainer persistentDataContainer);
 
